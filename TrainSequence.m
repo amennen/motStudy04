fd5d8c4ed90c20cf4948a29409bb660ff0c615c5
normanlab <amennen@princeton.edu>
2017-04-19 18:23:41 -0400

    subject 4 day 1

--- TrainSequence.m
+++ TrainSequence.m
@@ -1,7 +1,7 @@
 base_path = [fileparts(which('mot_realtime04MB.m')) filesep];
 cd(base_path);
 
-SUBJECT = 3;
+SUBJECT = 4;
 
 
 NUM_TASK_RUNS = 3;
@@ -39,7 +39,7 @@ DESCRIPTION = RECALL2 + 1; %26
 ASSOCIATES = DESCRIPTION + 1; %27
 
 %% first practice set
-mot_realtime04MB(SUBJECT, 4, [], 0, 0);
+mot_realtime04MB(SUBJECT, SETUP, [], 0, 0);
 
 % this will continue to train test and practice MOT, then move on to
 % MOT_Practice, MOT_PREP
